1895914d814210fce1a1cbcec573dc16841d8bc7
leafsoar <kltwjt@gmail.com>
2015-06-25 20:54:00 +0800

    update

--- init.el
+++ init.el
@@ -144,7 +144,7 @@ that was stored with ska-point-to-register."
 
 ;; 插件源
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("tromey" . "tromey.com/elpa/")
+						 ("tromey" . "tromey.com/elpa/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 ;; (setq package-enable-at-startup nil) 
@@ -204,6 +204,32 @@ that was stored with ska-point-to-register."
 (define-key company-active-map (kbd "\C-p") 'company-select-previous)
 (define-key company-active-map (kbd "\C-d") 'company-show-doc-buffer)
 ;; (define-key company-active-map (kbd "\M-/") 'company-complete)
+
+(require 'company-go)
+(setq company-tooltip-limit 20)                      ; bigger popup window
+(setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
+(setq company-echo-delay 0)                          ; remove annoying blinking
+(setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
+
+(add-hook 'go-mode-hook (lambda ()
+                          (set (make-local-variable 'company-backends) '(company-go))
+                          (company-mode)))
+
+;; (custom-set-faces
+;;  '(company-preview
+;;    ((t (:foreground "darkgray" :underline t))))
+;;  '(company-preview-common
+;;    ((t (:inherit company-preview))))
+;;  '(company-tooltip
+;;    ((t (:background "lightgray" :foreground "black"))))
+;;  '(company-tooltip-selection
+;;    ((t (:background "steelblue" :foreground "white"))))
+;;  '(company-tooltip-common
+;;    ((((type x)) (:inherit company-tooltip :weight bold))
+;;     (t (:inherit company-tooltip))))
+;;  '(company-tooltip-common-selection
+;;    ((((type x)) (:inherit company-tooltip-selection :weight bold))
+;;     (t (:inherit company-tooltip-selection)))))
 
 ;; (add-to-list 'company-backends 'company-dabbrev t)
 ;; (add-to-list 'company-backends 'company-ispell t)
