39b1f6d1410d416fdc2f1356a67d8a987a938c51
leafsoar <kltwjt@gmail.com>
2015-06-29 15:59:43 +0800

    update

--- lisp/ls-packages.el
+++ lisp/ls-packages.el
@@ -43,6 +43,22 @@
 (add-hook 'prog-mode-hook 'fci-mode)
 (add-hook 'prog-mode-hook 'helm-projectile-on)
 (add-hook 'prog-mode-hook 'flycheck-mode)
+(add-hook 'prog-mode-hook 'highlight-tail-mode)
+(setq highlight-tail-colors
+          '(("black" . 0)
+            ("#bc2525" . 25)
+            ("purple" . 66)))
+(setq highlight-tail-steps '60)
+(setq highlight-tail-timer '0.05)
+
+;; helm
+(setq helm-quick-update                     t ; do not display invisible candidates
+      helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-buffers-fuzzy-matching           t ; fuzzy matching buffer names when non--nil
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
 
 ;; company-mode 与  fci-mode 冲突解决
 (defvar-local company-fci-mode-on-p nil)
